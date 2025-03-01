00714c764013caedac74293c89444286080d2eab
wallenjos01 <wallenjos01@gmail.com>
2025-03-01 14:24:06 -0700

    Revert to using default namespace for added registries for compatibility with Fabric API

--- src/main/java/org/wallentines/databridge/DataBridgeRegistries.java
+++ src/main/java/org/wallentines/databridge/DataBridgeRegistries.java
@@ -6,10 +6,8 @@ import net.minecraft.resources.ResourceLocation;
 
 public class DataBridgeRegistries {
 
-    public static final String NAMESPACE = "databridge";
-
-    public static final ResourceKey<Registry<StateObject<?>>> STATE_OBJECT = ResourceKey.createRegistryKey(ResourceLocation.tryBuild(NAMESPACE, "state_object"));
-    public static final ResourceKey<Registry<CommandDefinition>> COMMAND = ResourceKey.createRegistryKey(ResourceLocation.tryBuild(NAMESPACE, "command"));
-    public static final ResourceKey<Registry<JavaFunctionDefinition>> FUNCTION = ResourceKey.createRegistryKey(ResourceLocation.tryBuild(NAMESPACE, "function"));
+    public static final ResourceKey<Registry<StateObject<?>>> STATE_OBJECT = ResourceKey.createRegistryKey(ResourceLocation.withDefaultNamespace("state_object"));
+    public static final ResourceKey<Registry<CommandDefinition>> COMMAND = ResourceKey.createRegistryKey(ResourceLocation.withDefaultNamespace("command"));
+    public static final ResourceKey<Registry<JavaFunctionDefinition>> FUNCTION = ResourceKey.createRegistryKey(ResourceLocation.withDefaultNamespace("function"));
 
 }
