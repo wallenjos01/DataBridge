f536c84ab8af00b19e14ec187cdeecc4fb21f971
wallenjos01 <wallenjos01@gmail.com>
2026-03-25 16:47:23 -0600

    Update to 26.1 release. Use proper fabric permisisons api

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,9 +14,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:9.2.2")
     implementation("net.fabricmc:fabric-loom:1.15-SNAPSHOT")
-    implementation("org.gradle.toolchains.foojay-resolver-convention:org.gradle.toolchains.foojay-resolver-convention.gradle.plugin:1.0.0")
 }
 
-//plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
-//}
